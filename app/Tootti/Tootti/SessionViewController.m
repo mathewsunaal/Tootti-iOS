2fd112be02ceabe403520f5a046517c7af18dd02
mathewsunaal <sunaalm@gmail.com>
2021-02-28 10:19:15 -0500

    Setup AVAudioSession to record in SessionVC

--- app/Tootti/Tootti/SessionViewController.m
+++ app/Tootti/Tootti/SessionViewController.m
@@ -20,28 +20,80 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self setupAVSession];
 }
 
+-(void) setupAVSession {
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"testAudio.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+ 
+    // Define AVAudioRecorder settings
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    self.audioRecorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    self.audioRecorder.delegate = self;
+    self.audioRecorder.meteringEnabled = YES;
+    [self.audioRecorder prepareToRecord];
+    
+}
+
+
+// Button action methods
 - (IBAction)recordAudio:(UIButton *)sender {
-    NSLog(@"Audio recording initiated");
+    NSLog(@"Audio recording pressed");
+    
+    if(self.audioPlayer.playing) {
+        [self.audioPlayer stop];
+    }
+    
+    if (!self.audioRecorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        
+        // Start recording
+        [self.audioRecorder record];
+        
+        // Update button
+        [sender setBackgroundImage:[UIImage systemImageNamed:@"stop.circle"] forState:UIControlStateNormal];
+    }
+    else {
+            
+        // Stop recording
+        // NOTE: we can also implement pause here and stop separately
+        [self.audioRecorder stop];
+            
+        // Update button
+        [sender setBackgroundImage:[UIImage systemImageNamed:@"record.circle"] forState:UIControlStateNormal];
+            
+        // Deactivate audio session
+        AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+        [audioSession setActive:NO error:nil];
+    }
 }
 
-- (IBAction)endAudioRecord:(UIButton *)sender {
-    NSLog(@"Audio recording ended");
-}
 
 - (IBAction)playAudio:(UIButton *)sender {
     NSLog(@"Audio playback initiated");
+    
+    if (!self.audioRecorder.recording){
+        self.audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:self.audioRecorder.url error:nil];
+        [self.audioPlayer setDelegate:self];
+        [self.audioPlayer play];
+    }
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
